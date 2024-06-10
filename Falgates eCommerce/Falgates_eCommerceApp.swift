61e25004233d6676c95637b176ab85d3087852b9
xBukhaari <akbukharisani@gmail.com>
2024-06-10 11:15:17 +0100

    Initial Commit

---
+++ Falgates eCommerce/Falgates_eCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Falgates_eCommerceApp.swift
+//  Falgates eCommerce
+//
+//  Created by Bukhaari on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Falgates_eCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
